70db3008de3cdd449f1a1d20ffceb7655bc45eb0
Matt <mnabavi84@users.noreply.github.com>
2020-08-07 17:07:58 -0700

    Add files via upload

---
+++ 01-Hello Python!.py
@@ -0,0 +1,27 @@
+# Example, do not modify!
+print(5 / 8)
+
+# Print the sum of 7 and 10
+print(7+10)
+
+
+# Division
+print(5 / 8)
+
+# Addition
+print(7 + 10)
+
+
+# Addition, subtraction
+print(5 + 5)
+print(5 - 5)
+
+# Multiplication, division, modulo, and exponentiation
+print(3 * 5)
+print(10 / 2)
+print(18 % 7)
+print(4 ** 2)
+
+print(100 * (1.1**7))
+
+
---
+++ 10-2D Numpy Arrays.py
@@ -0,0 +1,69 @@
+# Create baseball, a list of lists
+baseball = [[180, 78.4],
+            [215, 102.7],
+            [210, 98.5],
+            [188, 75.2]]
+
+# Import numpy
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the type of np_baseball
+print(type(np_baseball))
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create a 2D numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the shape of np_baseball
+print(np_baseball.shape)
+
+
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (2 cols)
+np_baseball = np.array(baseball)
+
+# Print out the 50th row of np_baseball
+print(np_baseball[49, :])
+
+# Select the entire second column of np_baseball: np_weight_lb
+np_weight_lb = np_baseball[:,1]
+
+# Print out height of 124th player
+print(np_baseball[123, 0])
+
+
+# baseball is available as a regular list of lists
+# updated is available as 2D numpy array
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and updated
+print(np_baseball+updated)
+
+# Create numpy array: conversion
+conversion = [0.0254, 0.453592, 1]
+
+# Print out product of np_baseball and conversion
+print(np_baseball * conversion)
+
+
+
